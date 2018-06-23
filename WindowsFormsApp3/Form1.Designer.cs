e6067c55923410037031731d0d83b02788240247
XxZ4CK50xX <34183092+XxZ4CK50xX@users.noreply.github.com>
2018-06-23 01:56:01 -0700

    therre

--- WindowsFormsApp3/Form1.Designer.cs
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -35,11 +35,11 @@ namespace WindowsFormsApp3
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.button3 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -66,16 +66,6 @@ namespace WindowsFormsApp3
             // 
             this.timer1.Interval = 70;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(15, 300);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(526, 28);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Make Preset";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.Button3_Click_1);
             // 
             // label1
             // 
@@ -113,16 +103,26 @@ namespace WindowsFormsApp3
             this.richTextBox1.TabIndex = 6;
             this.richTextBox1.Text = "";
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(15, 301);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(526, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Make Preset";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 411);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -139,11 +139,12 @@ namespace WindowsFormsApp3
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Button button3;
+ 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
